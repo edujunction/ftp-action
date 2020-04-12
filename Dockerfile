55fedb1ed464a4252ceab534fb3dccb95607a1e9
Tim Bernhard <tim@bernhard-webstudio.ch>
2020-04-12 21:04:18 +0200

    Fix issue where the local dir was copied too

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine:3.11
 
 COPY LICENSE README.md /
 
-RUN apk --no-cache add lftp
+# RUN apk --no-cache add lftp
 RUN apk --no-cache add ncftp
 
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -l
 
-
-
 # lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; mirror --reverse --continue --dereference --verbose=3 -P -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
 
-ncftpput -R -z -u "$INPUT_USER" -p "$INPUT_PASSWORD" "$INPUT_HOST" "$INPUT_REMOTEDIR" "$INPUT_LOCALDIR" 
+# switch directory instead of using ncftp "local_dir" parameter
+# this way, the local_dir is not created too.
+cd "$INPUT_LOCALDIR" || exit 2;
+ncftpput -R -z -m -u "$INPUT_USER" -p "$INPUT_PASSWORD" "$INPUT_HOST" "$INPUT_REMOTEDIR" "."
